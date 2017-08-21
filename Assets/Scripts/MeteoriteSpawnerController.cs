048cfc823c571485f65a977379afa29862f35bd8
awesomealvin <fookinhellvin@outlook.com>
2017-08-21 18:26:00 +1200

    Meteorite Spawning
    ADDED:
    - Added circle sprite (temporary) and assigned sprite to new meteorite object (prefab)
    - Added new tag: "Meteorite"
    - Added new layer: "Meteorite"
    - Added meteorite spawner
    	- Spawns meteorites at random location within it's collider
    	- Spawns at random forced angle (needs to be tested and modified heavily)
    - Added meteorite despawner
    	- Despawns meteorites that touches the despawner's collider.
    
    CHANGES:
    - Disabled meteorite collisions with each other via Project Settings > Physics 2D
    - Made walls prefab and modified in scene walls as appropriate
    - Moved walls up a bit

---
+++ Assets/Scripts/MeteoriteSpawnerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteSpawnerController : MonoBehaviour {
+
+	private Collider2D cd;
+	public GameObject meteorite;
+	
+	private float startSpawnTime = 1.0f;
+	public float timeBetweenSpawn = 0.4f;
+
+
+	// Use this for initialization
+	void Start () {
+		cd = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= startSpawnTime) {
+			SpawnMeteorites();
+			startSpawnTime = Time.time + timeBetweenSpawn;
+		}
+	}
+
+	void SpawnMeteorites() {
+		Bounds spawnBounds = cd.bounds;
+
+		Vector3 min = spawnBounds.min; // Get the minimum values
+		Vector3 max = spawnBounds.max; // Get the maximum values
+
+		// Randomize a position within the spawn area
+		float x = Random.Range(min.x, max.x);
+		float y = Random.Range(min.y, max.y);
+
+		// Instantiate the randomized location
+		Vector2 spawnLocation = new Vector3(x, y, 0);
+
+		// Create the object in the random position
+		GameObject spawned = Instantiate(meteorite, spawnLocation, Quaternion.identity);
+		Rigidbody2D rb = spawned.GetComponent<Rigidbody2D>();
+
+		// Randomize Angle
+		float angle = Random.Range(0, 359);
+		Vector3 dir = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.right;
+		rb.AddForce(dir * 150);
+	}
+}
