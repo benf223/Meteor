8d87601e9c185ec8f47f5cc5c599dd84f90bfde2
Jethro Tuburan <30586644+OrpheusTelos69@users.noreply.github.com>
2017-08-24 15:44:56 +1200

    End Game
    Village dissappears with the meteorite that hits it

---
+++ Assets/Scripts/VillageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageController : MonoBehaviour {
+
+    public bool isDestroyed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Meteorite"))
+        {
+            MeteoriteController meteorite = collision.gameObject.GetComponent<MeteoriteController>();
+            meteorite.BlowUp();
+            isDestroyed = true;
+            Destroy(gameObject);
+        }
+    }
+}
