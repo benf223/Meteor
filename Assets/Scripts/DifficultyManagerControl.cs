61d2a3d44f8422e198481d823ff837f621329c20
BukalaBunjoviBuckBeeBullaBeliBuu's Desktop <dhy9054@autuni.ac.nz>
2017-09-11 18:54:37 +1200

    Update timer
    timing is updated

---
+++ Assets/Scripts/DifficultyManagerControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyManagerControl : MonoBehaviour
+{
+    public float startTimer;
+    public Text timerText;
+    public bool timeFlowing;
+
+    // Use this for initialization
+    void Start()
+    {
+        timerText.text = "0";
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeFlowing)
+        {
+
+        }
+    }
+
+    void StartTimer()
+    {
+        startTimer = Time.time;
+        float countTime = Time.time - startTimer;
+
+        //string hours = 
+        string minutes = ((int)countTime / 99).ToString("f0");
+        string seconds = (countTime % 99).ToString("f0");
+
+        if (System.Convert.ToInt32(minutes) < 10)
+        {
+            timerText.text = "0";
+        }
+
+        if (System.Convert.ToInt32(seconds) < 10)
+        {
+            timerText.text += minutes + "0" + seconds;
+        }
+        else
+        {
+            timerText.text += minutes + seconds;
+        }
+    }
+
+    void PauseTimer()
+    {
+        timeFlowing = false;
+    }
+
+    void ResumeTimer()
+    {
+        timeFlowing = true;
+    }
+
+    void StopTimer()
+    {
+        timeFlowing = false;
+    }
+
+    void RestartTimer()
+    {
+        StopTimer();
+        StartTimer();
+    }
+
+}
