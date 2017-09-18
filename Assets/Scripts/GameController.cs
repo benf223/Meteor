12f6203fc4f26202fef9cb42d072a8512794f554
Jethro Tuburan <30586644+OrpheusTelos69@users.noreply.github.com>
2017-09-18 18:53:05 +1200

    Reset time

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,27 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     int score = 0;
+    bool gameOver;
     public Text gameOverText;
+    public float resetDelay;
+    float waitStartTime;
 
     // Use this for initialization
     void Start()
     {
+        gameOver = false;
         gameOverText.gameObject.SetActive(false);
-        StartCoroutine(MyCoroutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //current time is bigger then destroyed time +5 secs (5 second delay)
+        //and gameOver is true
+        if (gameOver && Time.time > waitStartTime + resetDelay)
+        {
+            ResetGame();
+        }
     }
 
     public void AddScore()
@@ -33,17 +41,16 @@ public class GameController : MonoBehaviour
         Debug.Log("Score: " + score);
     }
 
-    IEnumerator MyCoroutine()
-    {
-        yield return new WaitForSeconds(10);
-    }
-
     public void GameOver()
     {
+        gameOver = true;
         gameOverText.gameObject.SetActive(true);
-        // float waitStartTime = Time.time;
-        
+        waitStartTime = Time.time;//when the village is destroyed
+       
+    }
 
+    private void ResetGame()
+    {
         Application.LoadLevel(Application.loadedLevel);
     }
 }
