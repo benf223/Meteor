02b08d431d9ccc52750963eccb256292fc049ecd
benf223 <ppj1483@autuni.ac.nz>
2017-09-21 08:51:32 +1200

    Attempted Timing bug fix

--- Assets/Scripts/Controllers/Game Controllers/DifficultyManagerController.cs
+++ Assets/Scripts/Controllers/Game Controllers/DifficultyManagerController.cs
@@ -2,147 +2,162 @@
 
 public class DifficultyManagerController : MonoBehaviour
 {
-    // condition to keep the time continuing
-    public bool timeFlowing;
+	// condition to keep the time continuing
+	public bool timeFlowing;
+
 	public GameController gameCont;
-	
-    // time
-    private int hours;
-    private int minutes;
-    private int seconds;
+
+	// time
+	private int hours;
+
+	private int minutes;
+	private int seconds;
 	private float startTimer;
 	private float countTime;
-    private int interval = 1; // Time to update (seconds)
-    private float elapsedTime;
-    private int secondsToIncreaseDifficulty;
+	private int interval = 1; // Time to update (seconds)
+	private float elapsedTime;
+	private int secondsToIncreaseDifficulty;
 
-    // used to minus the duration time frames
-    private int timeRemoved;
-    private float spawnDelayMultiplier;
-    private float speedMultiplier;
-    private bool meteorSpawnDelayDifficultyUpdated;
+	// used to minus the duration time frames
+	private int timeRemoved;
 
-    // Use this for initialization
-    void Start()
-    {
-        timeFlowing = false;
-        secondsToIncreaseDifficulty = 0;
-        timeRemoved = 0;
-        spawnDelayMultiplier = 1.0f;
-        speedMultiplier = 1.0f;
-        meteorSpawnDelayDifficultyUpdated = false;
-    }
+	private float spawnDelayMultiplier;
+	private float speedMultiplier;
+	private bool meteorSpawnDelayDifficultyUpdated;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (timeFlowing)
-        {
-            UpdateTime();
-            DifficultyTimeFrame();
+	// Use this for initialization
+	void Start()
+	{
+		hours = 0;
+		minutes = 0;
+		seconds = 0;
+		elapsedTime = 0f;
+		
+		timeFlowing = false;
+		secondsToIncreaseDifficulty = 0;
+		timeRemoved = 0;
+		spawnDelayMultiplier = 1.0f;
+		speedMultiplier = 1.0f;
+		meteorSpawnDelayDifficultyUpdated = false;
+	}
 
-            // Updates every second (or based on interval variable)
-            if (Time.time >= elapsedTime) {
-                elapsedTime += interval;
-                UpdateEverySecond();
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (timeFlowing)
+		{
+			UpdateTime();
+			DifficultyTimeFrame();
 
-    // Called Every Second
-    private void UpdateEverySecond() {
-	    gameCont.AddScore(1);
-        secondsToIncreaseDifficulty++; // Used for the difficulty increase interval
-    }
+			// Updates every second (or based on interval variable)
+			if (Time.time >= elapsedTime)
+			{
+				elapsedTime += interval;
+				UpdateEverySecond();
+			}
+		}
+	}
 
-    void DifficultyTimeFrame()
-    {
-        // does 20, 19, 18, 17, 16, 15 all the way to 10 seconds it changes difficulty
-        // Note: Default values (20 - timeRemoved), (timeRemoved <= 10)
-        if (secondsToIncreaseDifficulty == 20 - timeRemoved)
-        {
-            if (timeRemoved <= 10)
-            {
-                timeRemoved++;
-            }
+	// Called Every Second
+	private void UpdateEverySecond()
+	{
+		gameCont.AddScore(1);
+		secondsToIncreaseDifficulty++; // Used for the difficulty increase interval
+	}
 
-            // Modify spawn delay multiplier
-            // NOTE THIS CAN CHANGE
-            // this decreases from 1 to 0.25 using 0.05 for every difficulty time frame.
-            // To further optimize, this section should call a method from the spawner
-            // Alvin will be keen to refactor these sections of code after main game is completed
-            if (spawnDelayMultiplier >= 0.25f) {
-                spawnDelayMultiplier -= 0.15f;
-            }
+	void DifficultyTimeFrame()
+	{
+		// does 20, 19, 18, 17, 16, 15 all the way to 10 seconds it changes difficulty
+		// Note: Default values (20 - timeRemoved), (timeRemoved <= 10)
+		if (secondsToIncreaseDifficulty == 20 - timeRemoved)
+		{
+			if (timeRemoved <= 10)
+			{
+				timeRemoved++;
+			}
 
-            // Modify meteorite speed multiplier
-            // NOTE THIS CAN CHANGE
-            // this increases from 1.0 to 2.0 using 0.05 for every difficulty time frame.
-            if (speedMultiplier <= 2.0f) {
-                speedMultiplier += 0.05f;
-            }
+			// Modify spawn delay multiplier
+			// NOTE THIS CAN CHANGE
+			// this decreases from 1 to 0.25 using 0.05 for every difficulty time frame.
+			// To further optimize, this section should call a method from the spawner
+			// Alvin will be keen to refactor these sections of code after main game is completed
+			if (spawnDelayMultiplier >= 0.25f)
+			{
+				spawnDelayMultiplier -= 0.15f;
+			}
 
-            secondsToIncreaseDifficulty = 0; // Reset countdown timer for difficulty change
-            meteorSpawnDelayDifficultyUpdated = true; // Notifies that difficulty has changed
-        }
-    }
+			// Modify meteorite speed multiplier
+			// NOTE THIS CAN CHANGE
+			// this increases from 1.0 to 2.0 using 0.05 for every difficulty time frame.
+			if (speedMultiplier <= 2.0f)
+			{
+				speedMultiplier += 0.05f;
+			}
 
-    // Used only for the MeteoriteSpawnController.
-    // Checks if difficulty has been updated, then resets status onced used
-    public bool MeteorSpawnDelayDifficultyUpdated() {
-        if (meteorSpawnDelayDifficultyUpdated) {
-            meteorSpawnDelayDifficultyUpdated = false;
-            return true;
-        }
-	    
+			secondsToIncreaseDifficulty = 0; // Reset countdown timer for difficulty change
+			meteorSpawnDelayDifficultyUpdated = true; // Notifies that difficulty has changed
+		}
+	}
+
+	// Used only for the MeteoriteSpawnController.
+	// Checks if difficulty has been updated, then resets status onced used
+	public bool MeteorSpawnDelayDifficultyUpdated()
+	{
+		if (meteorSpawnDelayDifficultyUpdated)
+		{
+			meteorSpawnDelayDifficultyUpdated = false;
+			return true;
+		}
+
 		return false;
-    }
+	}
 
-    
-    public float GetMeteoriteSpawnDelayMultiplier()
-    {
-        return spawnDelayMultiplier;
-    }
 
-    
-    public float GetMeteoriteSpeedMultiplier()
-    { 
-        return speedMultiplier;
-    }
+	public float GetMeteoriteSpawnDelayMultiplier()
+	{
+		return spawnDelayMultiplier;
+	}
 
-    public void StartTimer()
-    {
-        timeFlowing = true;
-        startTimer = Time.time;
-    }
 
-    public void UpdateTime()
-    {
-        countTime = Time.time - startTimer;
+	public float GetMeteoriteSpeedMultiplier()
+	{
+		return speedMultiplier;
+	}
 
-        hours = (int) countTime / 360;
-        minutes = (int) countTime / 60;
-        seconds = (int) countTime % 60;
-    }
+	public void StartTimer()
+	{
+		timeFlowing = true;
+		startTimer = Time.time;
+	}
 
-    public void PauseTimer()
-    {
-        timeFlowing = false;
-    }
+	public void UpdateTime()
+	{
+		countTime = Time.time - startTimer;
 
-    public void ResumeTimer()
-    {
-        timeFlowing = true;
-        startTimer = Time.time - countTime;
-    }
+		hours = (int) countTime / 360;
+		minutes = (int) countTime / 60;
+		seconds = (int) countTime % 60;
+	}
 
-    public void ResetTimer()
-    {
-        timeFlowing = false;
-        countTime = 0;
-    }
-	
-	public int GetSeconds() {
+	public void PauseTimer()
+	{
+		timeFlowing = false;
+	}
+
+	public void ResumeTimer()
+	{
+		timeFlowing = true;
+		startTimer = Time.time - countTime;
+	}
+
+	public void ResetTimer()
+	{
+		timeFlowing = false;
+		countTime = 0;
+	}
+
+	public int GetSeconds()
+	{
 		return seconds;
 	}
 }
