ae1f05a53366a7c4adc029418bb930cc32a608e9
awesomealvin <fookinhellvin@outlook.com>
2017-09-25 17:56:59 +1300

    Added Spawning Tests

--- Assets/Scripts/Controllers/MeteoriteSpawnerController.cs
+++ Assets/Scripts/Controllers/MeteoriteSpawnerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MeteoriteSpawnerController : MonoBehaviour
 {
+	public int amountSpawned {get; private set;}
+
 	private Collider2D cd;
 	public GameObject meteorite;
 	public GameObject diffucltyManager;
 	private DifficultyManagerController difficultyManagerController;
+
+	public bool spawningEnabled = true;
 
 	// Breath time for when to start the spawning of meteorites
 	private float startSpawnTime = 0.75f;
@@ -19,10 +23,15 @@ public class MeteoriteSpawnerController : MonoBehaviour
 	public float minSpawnForce;
 	public float maxSpawnForce;
 
+	// Testing tthe time for first spawn
+	private float startTime;
+	private float timeFirstSpawn = -1;
+
 
 	// Use this for initialization
 	private void Start()
 	{
+		startTime = Time.timeSinceLevelLoad;
 		Debug.Log("Spawn Rate: " + timeBetweenSpawn);
 		cd = GetComponent<BoxCollider2D>();
 		difficultyManagerController = diffucltyManager.GetComponent<DifficultyManagerController>();
@@ -51,6 +60,14 @@ public class MeteoriteSpawnerController : MonoBehaviour
 		{
 			SpawnMeteorites();
 			startSpawnTime = Time.timeSinceLevelLoad + timeBetweenSpawn;
+
+			/**
+			 * For Testing
+			 */
+			if (timeFirstSpawn == -1) {
+				timeFirstSpawn = Time.timeSinceLevelLoad;
+				Debug.Log("Delay for first spawN: " + (timeFirstSpawn - startTime));
+			}
 		}
 	}
 
@@ -75,6 +92,7 @@ public class MeteoriteSpawnerController : MonoBehaviour
 		// Create the object in the random position
 		GameObject spawned = Instantiate(meteorite, spawnLocation, Quaternion.identity);
 		Rigidbody2D rb = spawned.GetComponent<Rigidbody2D>();
+		// Debug.Log("Meteorite ID: " + spawned.GetInstanceID()+" | Spawn Location (x, y): " + "("+rb.position.x+". "+ rb.position.y+")");
 
 		// Randomize between spawning to the left, or the right
 		// 0 = right, 180 = left
