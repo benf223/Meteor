e46f0342d80adade59c05267d33523d1912e170d
BukalaBunjoviBuckBeeBullaBeliBuu's Desktop <dhy9054@autuni.ac.nz>
2017-10-10 19:07:31 +1300

    Shield update
    - Has visual effects
    - Yellow indication of warning
    - Red indication of warning
    - Health amount for shield
    - Needs to be merged into alvins branch

--- Assets/Scripts/Controllers/ShieldController.cs
+++ Assets/Scripts/Controllers/ShieldController.cs
@@ -6,40 +6,88 @@ public class ShieldController : MonoBehaviour
 {
 
     private GameObject shield;
-    private float timer;
-    private float deltaTimer;
+    private float startTimer;
+    private float timeSinceStart;
+    private int counter;
+    private int shieldHealth;
+
+    Color blue;
+    Color yellow;
+    Color red;
 
     // Use this for initialization
     void Start()
     {
         shield = GameObject.Find("Shield");
-        timer = Time.time;
-        deltaTimer = Time.deltaTime;
+
+        startTimer = Time.timeSinceLevelLoad;
+        timeSinceStart = 0;
+        counter = 0;
+        shieldHealth = 100;
+
+        blue = new Color(0.0f, 0.95f, 1.0f);
+        yellow = new Color(1.0f, 1.0f, 0);
+        red = new Color(1.0f, 0.2f, 0.2f);
+        shield.GetComponent<Renderer>().material.color = blue;
+
+        InvokeRepeating("CautionOfShield", 5.0f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // invoke repeating must be limited on the duration
-        // must do timer and has to be less than the duration of 10 seconds
-        // when the 5 seconds is up the flashes will do it more often (blue to yellow)
-        // when 8 seconds is up the flash will increase even more (blue to red)
-        CancelInvoke();
+        // destory shield after 10 seconds
+        timeSinceStart = Time.timeSinceLevelLoad - startTimer;
+        if (timeSinceStart > 10 || shieldHealth == 0)
+        {
+            Destroy(shield);
+        }
     }
 
-    private void changeColour(int counter)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Meteorite"))
+        {
+            shieldHealth -= 50;
+        }
+    }
+
+    public void CautionOfShield()
+    {
+        Indicator();
+        if (timeSinceStart > 8)
+        {
+            CancelInvoke();
+            counter = 0;
+            InvokeRepeating("Indicator", 0, 0.25f);
+        }
+        if (timeSinceStart > 10)
+        {
+            CancelInvoke();
+        }
+    }
+
+    public void Indicator()
     {
         if (counter == 0)
         {
-            shield.GetComponent<Renderer>().material.color = Color.blue;
+            if (timeSinceStart > 8)
+            {
+                Debug.Log("Red " + timeSinceStart);
+                shield.GetComponent<Renderer>().material.color = red;
+            }
+            else
+            {
+                Debug.Log("Yellow " + timeSinceStart);
+                shield.GetComponent<Renderer>().material.color = yellow;
+            }
+            counter++;
         }
         else if (counter == 1)
         {
-            shield.GetComponent<Renderer>().material.color = Color.yellow;
-        }
-        else if (counter == 2)
-        {
-            shield.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("Blue " + timeSinceStart);
+            shield.GetComponent<Renderer>().material.color = blue;
+            counter--;
         }
     }
 }
