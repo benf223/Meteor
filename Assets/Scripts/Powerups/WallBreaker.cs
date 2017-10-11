867d8b107bc11e53fb716fe34fb2717cd6cc204d
benf223 <ppj1483@autuni.ac.nz>
2017-10-12 10:57:53 +1300

    Wall Breaking powerup

---
+++ Assets/Scripts/Powerups/WallBreaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBreaker : Powerup
+{
+	private GameObject[] walls;
+
+	private float timeSinceStart;
+
+	protected override void ActivatePowerup()
+	{
+		walls = GameObject.FindGameObjectsWithTag("Wall");
+		
+		foreach (GameObject a in walls)
+		{
+			a.SetActive(false);
+		}
+	}
+
+	protected override void DeactivatePowerup()
+	{
+		foreach (GameObject a in walls)
+		{
+			a.SetActive(true);
+		}
+	}
+}
