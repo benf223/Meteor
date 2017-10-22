ae821d76e3fdf4e71ece7affe8c7f9c55fd40e44
callibates <fcf0178@aut.ac.nz>
2017-10-22 22:35:33 +1300

    Working on our lovely cannon

---
+++ Assets/CannonBallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallController : MonoBehaviour {
+	private Rigidbody2D rb;
+	public CannonController cc;
+	private float xVal = 0;
+	// Use this for initialization
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Meteorite"))
+		{
+			Destroy(collision.gameObject);
+			MeteoriteController meteorite = collision.gameObject.GetComponent<MeteoriteController>();
+			Destroy (this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float t = cc.GetTime ();
+		//Debug.Log ("TimeD: "+t);
+
+		xVal = cc.GetTime ();
+		Debug.Log ("Get" + xVal);
+		rb = GetComponent<Rigidbody2D> ();
+		Vector2 v = new Vector2(xVal, 100.0f);
+		rb.AddForce(v);
+	}
+	public void Shoot() {
+	}
+}
---
+++ Assets/Scripts/Powerups/CannonController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class CannonController : Powerup
+{
+	private GameObject cannon;
+	public GameObject cannonBall;
+	public GameObject wheel;
+	//rpublic Transform spawn;
+	private float timeSinceStart;
+	//private float timer = 0;
+	private float keepTrack = 0.2f;
+	private float rotateTime = 2.0f;
+	private Vector3 rotationDir = Vector3.forward * 10;
+	private GameObject thisWheel;
+	public CannonBallController cbc;
+
+	protected override void ActivatePowerup()
+	{
+		cannon = GetComponent<GameObject> ();
+		transform.position = new Vector2(0.0f, -4.25f);
+		Vector2 v2 = new Vector2 (0.1f, -4.1f);
+		thisWheel = (GameObject)Instantiate (wheel, v2, Quaternion.Euler (0f, 0f, 0f)) as GameObject;
+		//wheel.SetActive (true);
+		//wheel.transform.position = new Vector2 (0.16f, -4.1f);
+	}
+
+	protected override void DeactivatePowerup()
+	{
+		Destroy (cannon);
+		Destroy (thisWheel);
+	}
+
+	private new void Update()
+	{
+		UpdateVisibles();
+
+		timeSinceStart = Time.timeSinceLevelLoad - startTime;
+		if (timeSinceStart >= keepTrack) {
+			if (timeSinceStart >= rotateTime) {
+				rotateTime += 2;
+				rotationDir *= -1.0f;
+			}
+			transform.Rotate (rotationDir);
+			keepTrack += 0.2f;
+			Debug.Log ("SHOOT");
+			//cannonBall.transform.position = new Vector2 (0.0f, -4.109f);
+			Vector2 v = new Vector2 (0.0f, -4.109f);
+			GameObject thisBall;
+			thisBall = (GameObject)Instantiate(cannonBall, v, Quaternion.Euler(0.0f,0.0f,0.0f)) as GameObject;
+			cbc.Shoot ();
+			//thisBall.transform.Rotate (rotationDir);
+			//thisBall.rigidbody.AddForce(Vector3.forward * 10000);
+			//thisBall.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 10000);
+
+		}
+
+		if (Time.timeSinceLevelLoad - startTime >= duration)
+		{
+			DeactivatePowerup();
+			DeactivateSlider();
+			Destroy(gameObject);
+		}
+	}
+	public Vector3 CurrRot() {
+		Vector2 vBarrelDir = new Vector2(transform.up.x,transform.up.y);
+		return rotationDir;
+	}
+
+	public float GetTime() {
+		Debug.Log ("Called gettime())"+timeSinceStart);
+		return timeSinceStart;
+	}
+
+
+
+
+}
+
+
