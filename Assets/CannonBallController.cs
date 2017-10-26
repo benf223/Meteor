16545fc958781db8babc8b20e8cf7e96fe95c438
callibates <fcf0178@aut.ac.nz>
2017-10-26 17:26:30 +1300

    thecannonshootingblehblehblehblehbleh

--- Assets/CannonBallController.cs
+++ Assets/CannonBallController.cs
@@ -6,6 +6,12 @@ public class CannonBallController : MonoBehaviour {
 	private Rigidbody2D rb;
 	public CannonController cc;
 	private float xVal = 0;
+
+	[HideInInspector]
+	public bool constant = false;
+	public float speed;
+	public Vector3 direction;
+
 	// Use this for initialization
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
@@ -15,19 +21,41 @@ public class CannonBallController : MonoBehaviour {
 			MeteoriteController meteorite = collision.gameObject.GetComponent<MeteoriteController>();
 			Destroy (this.gameObject);
 		}
+		if(collision.gameObject.CompareTag("Wall") )
+		{
+				Destroy (this.gameObject);
+		}
 	}
-	
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
 	// Update is called once per frame
 	void Update () {
-		//float t = cc.GetTime ();
-		//Debug.Log ("TimeD: "+t);
 
-		xVal = cc.GetTime ();
-		Debug.Log ("Get" + xVal);
-		rb = GetComponent<Rigidbody2D> ();
-		Vector2 v = new Vector2(xVal, 100.0f);
-		rb.AddForce(v);
 	}
+
+	void FixedUpdate() {
+		// Applies a constant velocity
+		if (constant) {
+			rb.velocity = direction * speed;
+		}
+	}
+
+	/**
+	 * Sets a constant speed for the balls.
+	 * @param speed The desired ball speed
+	 * @param direction The desired direction
+	 * Note: Should be called from the cannon.
+	 */
+	public void SetConstant(float speed, Vector3 direction) {
+		constant = true;
+		this.speed = speed;
+		this.direction = direction;
+	}
+
 	public void Shoot() {
 	}
 }
+
--- Assets/Scripts/Powerups/CannonController.cs
+++ Assets/Scripts/Powerups/CannonController.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 
-public class CannonController : Powerup
-{
+/**
+ * NOTES (things I did to make it work):
+ * 1. The sprite of your cannon must point UPWARDS as it's the default direction for sprites in Unity. So rotate it in GIMP or something.
+ * 2. Don't need that Constant Force component, as it's only for ONE direction. Better off changing the velocity in script.
+ * 3. Make sure the balls have gravity scale. I noticed you had it at 0 before. May not be needed for the constant method.
+ * 4. ANYTHING moving should have a Rigidbody component. Your cannon did not.
+ * 5. Anything physics related, use FixedUpdate instead of Update
+ */
+
+/**
+* OTHER NOTES:
+* 1) This is your code, so change/modify whatever fits you best :)
+* 2) Make sure to despawn the cannonballs after a period of time, or when it hits something like a wall or despawner.
+* 3) Check the CannonBallController if you haven't already
+	*/
+	public class CannonController : Powerup {
 	private GameObject cannon;
 	public GameObject cannonBall;
 	public GameObject wheel;
@@ -14,60 +28,65 @@ public class CannonController : Powerup
 	private GameObject thisWheel;
 	public CannonBallController cbc;
 
-	protected override void ActivatePowerup()
-	{
-		cannon = GetComponent<GameObject> ();
+	protected override void ActivatePowerup() {
+		cannon = GetComponent<GameObject>();
 		transform.position = new Vector2(0.0f, -4.25f);
-		Vector2 v2 = new Vector2 (0.1f, -4.1f);
-		thisWheel = (GameObject)Instantiate (wheel, v2, Quaternion.Euler (0f, 0f, 0f)) as GameObject;
+		Vector2 v2 = new Vector2(0.1f, -4.1f);
+		thisWheel = (GameObject)Instantiate(wheel, v2, Quaternion.Euler(0f, 0f, 0f)) as GameObject;
 		//wheel.SetActive (true);
 		//wheel.transform.position = new Vector2 (0.16f, -4.1f);
 	}
 
-	protected override void DeactivatePowerup()
-	{
-		Destroy (cannon);
-		Destroy (thisWheel);
+	protected override void DeactivatePowerup() {
+		Destroy(cannon);
+		Destroy(thisWheel);
 	}
 
-	private new void Update()
-	{
+	// Whenever physics is involed, use FixedUpdate :)
+	private void FixedUpdate() {
 		UpdateVisibles();
 
 		timeSinceStart = Time.timeSinceLevelLoad - startTime;
 		if (timeSinceStart >= keepTrack) {
 			if (timeSinceStart >= rotateTime) {
-				rotateTime += 2;
+				rotateTime += 2f;
 				rotationDir *= -1.0f;
 			}
-			transform.Rotate (rotationDir);
+
+			// Initialize dem components
+			Transform tf = GetComponent<Transform>();
+			Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+			// No idea how your rotation works, but okeh
+			tf.Rotate(rotationDir);
 			keepTrack += 0.2f;
-			Debug.Log ("SHOOT");
-			//cannonBall.transform.position = new Vector2 (0.0f, -4.109f);
-			Vector2 v = new Vector2 (0.0f, -4.109f);
+
+			Debug.Log("SHOOT");
+
 			GameObject thisBall;
-			thisBall = (GameObject)Instantiate(cannonBall, v, Quaternion.Euler(0.0f,0.0f,0.0f)) as GameObject;
-			cbc.Shoot ();
-			//thisBall.transform.Rotate (rotationDir);
-			//thisBall.rigidbody.AddForce(Vector3.forward * 10000);
-			//thisBall.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 10000);
+			thisBall = (GameObject)Instantiate(cannonBall, rb.position, Quaternion.identity); //as GameObject; <- Same as casting.
+			// Also, you can just use Quanterion.identity for default rotation
 
-		}
+			/* UNCOMMENT FOR CONSTANT SPEED AND COMMENT OTHER ONE BELOW
+			CannonBallController thisBallController = thisBall.GetComponent<CannonBallController>();
+			// See function for details...
+			thisBallController.SetConstant(10f, tf.up);
+			 */
 
-		if (Time.timeSinceLevelLoad - startTime >= duration)
-		{
-			DeactivatePowerup();
-			DeactivateSlider();
-			Destroy(gameObject);
+			// I like this one because physics, and realism... Cannon balls don't have constant force ;)
+			// Up to you though, it's your cannon haha
+			float force = 1000f;
+			thisBall.GetComponent<Rigidbody2D>().AddForce(tf.up * force);
+
 		}
 	}
 	public Vector3 CurrRot() {
-		Vector2 vBarrelDir = new Vector2(transform.up.x,transform.up.y);
+		Vector2 vBarrelDir = new Vector2(transform.up.x, transform.up.y);
 		return rotationDir;
 	}
 
 	public float GetTime() {
-		Debug.Log ("Called gettime())"+timeSinceStart);
+		Debug.Log("Called gettime())" + timeSinceStart);
 		return timeSinceStart;
 	}
 
@@ -75,5 +94,7 @@ public class CannonController : Powerup
 
 
 }
+
+
 
 
